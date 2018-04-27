b3076e352330469d2680926076199d55c66082b6
Javier Arias <javier@arias.re>
2018-04-27 13:26:38 +0100

    add uuid as event_id

--- api/models.py
+++ api/models.py
@@ -1,23 +1,26 @@
+import uuid
 import logging
 import psycopg2
 from errors import NotFound, NotAllowed
 
 class Event(object):
-    def __init__(self, uri, measure, timestamp, value, country, uploader):
+    def __init__(self, event_id, uri, measure, timestamp, value, country, uploader):
+        self.event_id  = str(event_id) if event_id else str(uuid.uuid4())
         self.URI       = str(uri)
         self.measure   = str(measure)
         self.timestamp = timestamp.strftime('%Y-%m-%dT%H:%M:%S%z')
         self.value     = int(value)
-        self.country   = str(country)
-        self.uploader  = int(uploader)
+        self.country   = str(country) if country else None
+        self.uploader  = str(uploader)
 
     def save(self, connection):
         try:
             c = connection.cursor()
-            c.execute('''INSERT INTO event (uri, measure_id, timestamp,
-                                            country_code, value, uploader_id)
-                         VALUES (%s, %s, %s, %s, %s, %s);''', \
-                      (self.URI, self.measure, self.timestamp, \
+            c.execute('''INSERT INTO event (event_id, uri, measure_id,
+                                            timestamp, country_id,
+                                            value, uploader_id)
+                         VALUES (%s, %s, %s, %s, %s, %s, %s);''', \
+                      (self.event_id, self.URI, self.measure, self.timestamp, \
                        self.country, self.value, self.uploader))
             success = connection.commit()
             c.close()
