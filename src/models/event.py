58f6d5f0125b0262c0b3d5c35241c07e086b9f7e
Javier Arias <javier@arias.re>
2019-08-14 17:27:27 +0100

    abstract query wrapper

--- src/models/event.py
+++ src/models/event.py
@@ -1,9 +1,4 @@
-import psycopg2
-from api import db
-from aux import logger_instance
-from errors import Error, FATAL
-
-logger = logger_instance(__name__)
+from .operations import do_query
 
 
 class Event():
@@ -19,21 +14,17 @@ class Event():
         self.uploader_uri = uploader_uri
 
     def save(self):
-        try:
-            q = '''INSERT INTO event (event_id, work_uri, measure_uri,
-                      timestamp, value, event_uri, country_uri, uploader_uri)
-                    VALUES ($event_id, $work_uri, $measure_uri, $timestamp,
-                      $value, $event_uri, $country_uri, $uploader_uri)
-                    ON CONFLICT DO NOTHING;'''
-            vals = dict(event_id=self.event_id, work_uri=self.work_uri,
-                        measure_uri=self.measure_uri, timestamp=self.timestamp,
-                        value=self.value, event_uri=self.event_uri,
-                        country_uri=self.country_uri,
-                        uploader_uri=self.uploader_uri)
-            db.query(q, vals)
-        except (Exception, psycopg2.DatabaseError) as error:
-            logger.debug(error)
-            raise Error(FATAL)
+        q = '''INSERT INTO event (event_id, work_uri, measure_uri,
+                  timestamp, value, event_uri, country_uri, uploader_uri)
+                VALUES ($event_id, $work_uri, $measure_uri, $timestamp,
+                  $value, $event_uri, $country_uri, $uploader_uri)
+                ON CONFLICT DO NOTHING;'''
+        vals = dict(event_id=self.event_id, work_uri=self.work_uri,
+                    measure_uri=self.measure_uri, timestamp=self.timestamp,
+                    value=self.value, event_uri=self.event_uri,
+                    country_uri=self.country_uri,
+                    uploader_uri=self.uploader_uri)
+        do_query(q, vals)
 
     @staticmethod
     def get_from_event_id(event_id):
@@ -43,175 +34,136 @@ class Event():
 
     @staticmethod
     def get_all(clause, params):
-        try:
-            q = '''SELECT * FROM event WHERE 1=1 ''' + clause + '''
-                   ORDER BY timestamp;'''
-            return db.query(q, params)
-        except (Exception, psycopg2.DatabaseError) as error:
-            logger.error(error)
-            raise Error(FATAL)
+        q = '''SELECT * FROM event WHERE 1=1 ''' + clause + '''
+                ORDER BY timestamp;'''
+        return do_query(q, params)
 
     @staticmethod
     def aggregate_by_measure(clause, params):
-        try:
-            q = '''SELECT
-                     measure_uri,
-                     namespace,
-                     source,
-                     type,
-                     version,
-                     SUM(value) as value
-                   FROM event INNER JOIN measure USING(measure_uri)
-                   WHERE 1=1 ''' + clause + '''
-                   GROUP BY measure_uri, namespace, source, type, version
-                   ORDER BY measure_uri;'''
-            result = db.query(q, params)
-            return result
-        except (Exception, psycopg2.DatabaseError) as error:
-            logger.error(error)
-            raise Error(FATAL)
+        q = '''SELECT
+                  measure_uri,
+                  namespace,
+                  source,
+                  type,
+                  version,
+                  SUM(value) as value
+                FROM event INNER JOIN measure USING(measure_uri)
+                WHERE 1=1 ''' + clause + '''
+                GROUP BY measure_uri, namespace, source, type, version
+                ORDER BY measure_uri;'''
+        return do_query(q, params)
 
     @staticmethod
     def aggregate_by_measure_country(clause, params):
-        try:
-            q = '''SELECT
-                     measure_uri,
-                     namespace,
-                     source,
-                     type,
-                     version,
-                     country_uri,
-                     country_code,
-                     country_name,
-                     continent_code,
-                     SUM(value) as value
-                   FROM event
-                     INNER JOIN measure USING(measure_uri)
-                     LEFT JOIN country USING(country_uri)
-                   WHERE 1=1 ''' + clause + '''
-                   GROUP BY measure_uri, namespace, source, type, version,
-                      country_uri, country_code, country_name, continent_code
-                   ORDER BY measure_uri, country_uri;'''
-            result = db.query(q, params)
-            return result
-        except (Exception, psycopg2.DatabaseError) as error:
-            logger.error(error)
-            raise Error(FATAL)
+        q = '''SELECT
+                  measure_uri,
+                  namespace,
+                  source,
+                  type,
+                  version,
+                  country_uri,
+                  country_code,
+                  country_name,
+                  continent_code,
+                  SUM(value) as value
+                FROM event
+                  INNER JOIN measure USING(measure_uri)
+                  LEFT JOIN country USING(country_uri)
+                WHERE 1=1 ''' + clause + '''
+                GROUP BY measure_uri, namespace, source, type, version,
+                  country_uri, country_code, country_name, continent_code
+                ORDER BY measure_uri, country_uri;'''
+        return do_query(q, params)
 
     @staticmethod
     def aggregate_by_country_measure(clause, params):
-        try:
-            q = '''SELECT
-                     country_uri,
-                     country_code,
-                     country_name,
-                     continent_code,
-                     measure_uri,
-                     namespace,
-                     source,
-                     type,
-                     version,
-                     SUM(value) as value
-                   FROM event
-                     INNER JOIN measure USING(measure_uri)
-                     LEFT JOIN country USING(country_uri)
-                   WHERE 1=1 ''' + clause + '''
-                   GROUP BY country_uri, country_code, country_name,
-                      continent_code, measure_uri, namespace, source,
-                      type, version
-                   ORDER BY country_uri, measure_uri;'''
-            result = db.query(q, params)
-            return result
-        except (Exception, psycopg2.DatabaseError) as error:
-            logger.error(error)
-            raise Error(FATAL)
+        q = '''SELECT
+                  country_uri,
+                  country_code,
+                  country_name,
+                  continent_code,
+                  measure_uri,
+                  namespace,
+                  source,
+                  type,
+                  version,
+                  SUM(value) as value
+                FROM event
+                  INNER JOIN measure USING(measure_uri)
+                  LEFT JOIN country USING(country_uri)
+                WHERE 1=1 ''' + clause + '''
+                GROUP BY country_uri, country_code, country_name,
+                  continent_code, measure_uri, namespace, source,
+                  type, version
+                ORDER BY country_uri, measure_uri;'''
+        return do_query(q, params)
 
     @staticmethod
     def aggregate_by_measure_year(clause, params):
-        try:
-            q = '''SELECT
-                     measure_uri,
-                     namespace,
-                     source,
-                     type,
-                     version,
-                     to_char(timestamp, 'YYYY') as year,
-                     SUM(value) as value
-                   FROM event
-                     INNER JOIN measure USING(measure_uri)
-                   WHERE 1=1 ''' + clause + '''
-                   GROUP BY measure_uri, namespace, source, type, version, year
-                   ORDER BY measure_uri, year;'''
-            result = db.query(q, params)
-            return result
-        except (Exception, psycopg2.DatabaseError) as error:
-            logger.error(error)
-            raise Error(FATAL)
+        q = '''SELECT
+                  measure_uri,
+                  namespace,
+                  source,
+                  type,
+                  version,
+                  to_char(timestamp, 'YYYY') as year,
+                  SUM(value) as value
+                FROM event
+                  INNER JOIN measure USING(measure_uri)
+                WHERE 1=1 ''' + clause + '''
+                GROUP BY measure_uri, namespace, source, type, version, year
+                ORDER BY measure_uri, year;'''
+        return do_query(q, params)
 
     @staticmethod
     def aggregate_by_year_measure(clause, params):
-        try:
-            q = '''SELECT
-                     to_char(timestamp, 'YYYY') as year,
-                     measure_uri,
-                     namespace,
-                     source,
-                     type,
-                     version,
-                     SUM(value) as value
-                   FROM event
-                     INNER JOIN measure USING(measure_uri)
-                   WHERE 1=1 ''' + clause + '''
-                   GROUP BY year, measure_uri, namespace, source, type, version
-                   ORDER BY year, measure_uri;'''
-            result = db.query(q, params)
-            return result
-        except (Exception, psycopg2.DatabaseError) as error:
-            logger.error(error)
-            raise Error(FATAL)
+        q = '''SELECT
+                  to_char(timestamp, 'YYYY') as year,
+                  measure_uri,
+                  namespace,
+                  source,
+                  type,
+                  version,
+                  SUM(value) as value
+                FROM event
+                  INNER JOIN measure USING(measure_uri)
+                WHERE 1=1 ''' + clause + '''
+                GROUP BY year, measure_uri, namespace, source, type, version
+                ORDER BY year, measure_uri;'''
+        return do_query(q, params)
 
     @staticmethod
     def aggregate_by_measure_month(clause, params):
-        try:
-            q = '''SELECT
-                     measure_uri,
-                     namespace,
-                     source,
-                     type,
-                     version,
-                     to_char(timestamp, 'MM') as month,
-                     SUM(value) as value
-                   FROM event
-                     INNER JOIN measure USING(measure_uri)
-                   WHERE 1=1 ''' + clause + '''
-                   GROUP BY measure_uri, namespace, source, type, version,
-                     month
-                   ORDER BY measure_uri, month;'''
-            result = db.query(q, params)
-            return result
-        except (Exception, psycopg2.DatabaseError) as error:
-            logger.error(error)
-            raise Error(FATAL)
+        q = '''SELECT
+                  measure_uri,
+                  namespace,
+                  source,
+                  type,
+                  version,
+                  to_char(timestamp, 'MM') as month,
+                  SUM(value) as value
+                FROM event
+                  INNER JOIN measure USING(measure_uri)
+                WHERE 1=1 ''' + clause + '''
+                GROUP BY measure_uri, namespace, source, type, version,
+                  month
+                ORDER BY measure_uri, month;'''
+        return do_query(q, params)
 
     @staticmethod
     def aggregate_by_month_measure(clause, params):
-        try:
-            q = '''SELECT
-                     to_char(timestamp, 'MM') as month,
-                     measure_uri,
-                     namespace,
-                     source,
-                     type,
-                     version,
-                     SUM(value) as value
-                   FROM event
-                     INNER JOIN measure USING(measure_uri)
-                   WHERE 1=1 ''' + clause + '''
-                   GROUP BY month, measure_uri, namespace, source, type,
-                     version
-                   ORDER BY month, measure_uri;'''
-            result = db.query(q, params)
-            return result
-        except (Exception, psycopg2.DatabaseError) as error:
-            logger.error(error)
-            raise Error(FATAL)
+        q = '''SELECT
+                  to_char(timestamp, 'MM') as month,
+                  measure_uri,
+                  namespace,
+                  source,
+                  type,
+                  version,
+                  SUM(value) as value
+                FROM event
+                  INNER JOIN measure USING(measure_uri)
+                WHERE 1=1 ''' + clause + '''
+                GROUP BY month, measure_uri, namespace, source, type,
+                  version
+                ORDER BY month, measure_uri;'''
+        return do_query(q, params)
