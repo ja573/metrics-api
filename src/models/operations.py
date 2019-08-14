969a87bbc6f89ebf8010d6f56d9ae72b6d838ca6
Javier Arias <javier@arias.re>
2019-08-14 17:31:35 +0100

    use pertinent imports

--- src/models/operations.py
+++ src/models/operations.py
@@ -2,25 +2,25 @@ import psycopg2
 from api import db
 from aux import logger_instance
 from errors import Error, FATAL
-from .event import Event
-from .country import Country
-from .measure import Measure
 
 logger = logger_instance(__name__)
 
 
 def result_to_event(r):
+    from .event import Event
     return Event(r["event_id"], r["work_uri"], r["measure_uri"],
                  r["timestamp"], r["value"], r["event_uri"],
                  r["country_uri"], r["uploader_uri"])
 
 
 def result_to_country(r):
+    from .country import Country
     return Country(r["country_uri"], r["country_code"], r["country_name"],
                    r["continent_code"])
 
 
 def result_to_measure(r, description=False):
+    from .measure import Measure
     measure = Measure(r["measure_uri"], r["namespace"], r["source"], r["type"],
                       r["version"])
     if description:
